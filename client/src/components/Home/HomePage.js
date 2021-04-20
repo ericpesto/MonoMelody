3095b53377556b3e72a0b1d8ed43980193507e66
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-04-20 22:09:33 +0100

    homepage

--- client/src/components/Home/HomePage.js
+++ client/src/components/Home/HomePage.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import React from 'react'
 import './homepage.scss'
 import Typing from 'react-typing-animation'
+import Particles from 'react-particles-js'
 
 const HomePage = () => {
 
@@ -9,31 +11,84 @@ const HomePage = () => {
 
   return (
     <>
-      <div className='homepage component'>
+      <div className="homepage-wrapper">
+        <div className="hero">
 
-        <header>
           <Typing>
             <Typing.Delay ms={1000} />
-            <div> Welcome to Mono<strong>Melody.</strong> 🎶</div>
+            <h1> Welcome to Mono<span className="hero-bold-text">Melody<span id="orange-dot">.</span></span></h1>
+            <Typing.Delay ms={500} />
+            <p className="hero-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis sem lacus, nec laoreet augue dignissim vitae. Nulla facilisi. Donec eu velit pretium nulla rutrum venenatis.</p>
+            <Typing.Delay ms={500} />
+            <button className="button">Create</button>
           </Typing>
           
-        </header>
-        <div className='homepage-content'>
+        </div>
+        <Particles
+          className="particles-wrapper"
+          params={{
+	    'particles': {
+	        'number': {
+	            'value': 8,
+	            'density': {
+	                'enable': true,
+	                'value_area': 800,
+	            },
+	        },
+	        'line_linked': {
+	            'enable': false,
+	        },
+	        'move': {
+	            'speed': 1,
+	            'out_mode': 'out',
+	        },
+	        'shape': {
+	            'type': [
+	                'image',
+	                'circle'
+	            ],
+	            'image': [
+	                {
+	                    'src': '/react.cd2ab268.svg',
+	                    'height': 20,
+	                    'width': 23,
+	                },
+	                {
+	                    'src': '/k8s.2d579d24.svg',
+	                    'height': 20,
+	                    'width': 20,
+	                },
+	                {
+	                    'src': '/code.b3b4c4f4.png',
+	                    'height': 20,
+	                    'width': 20,
+	                }
+	            ],
+	        },
+	        'color': {
+	            'value': '#ff7f08',
+	        },
+	        'size': {
+	            'value': 30,
+	            'random': false,
+	            'anim': {
+	                'enable': true,
+	                'speed': 4,
+	                'size_min': 10,
+	                'sync': false,
+	            },
+	        },
+	    },
+	    'retina_detect': false,
+          }} />
+        {/* <div className='homepage-content'>
           <p>
           
           </p>
-        </div>
+        </div> */}
 
 
       </div>
-
-
-
-   
- 
-
-
-
 
     </>
   )
