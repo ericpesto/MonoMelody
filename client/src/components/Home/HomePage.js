3f85c1029bb26a9c291a4cee6d2242ee4f247d67
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-04-21 20:44:49 +0100

    push

--- client/src/components/Home/HomePage.js
+++ client/src/components/Home/HomePage.js
@@ -7,9 +7,6 @@ import ParticlesBg from '../particles/ParticlesBg'
 
 const HomePage = () => {
 
-
-
-
   return (
     <>
       <div className="homepage-wrapper">
@@ -21,8 +18,10 @@ const HomePage = () => {
             <Typing.Delay ms={1000} />
             <h1> Welcome to Mono_<span className="hero-bold-text">Melody<span id="orange-dot">.</span></span></h1>
             <Typing.Delay ms={500} />
+            <Typing.Speed ms={25} />
             <p className="hero-text">monoMelody is a music minded social media platform. Use our bespoke sequencer to craft your own melodies and share them for others to enjoy.</p>
-            <Typing.Delay ms={500} />
+            <Typing.Delay ms={250} />
+            <Typing.Speed ms={100} />
             <Link to="/create" className="home-button">Create</Link>
           </Typing>
           
