058f5525a094435f4ec9a977890338b570fad937
hamisakim <samishakim@gmail.com>
2021-04-22 11:19:06 +0100

    final page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -2,33 +2,60 @@
 import React from 'react'
 import '../../styles/main.scss'
 import ParticlesBg from '../particles/ParticlesBg'
-
-
+import eric from '../../assets/Eric_Linkedin.jpeg'
+import sami from '../../assets/Sami_Linkedin.jpeg'
+import link from '../../assets/link.png'
+import git from '../../assets/Git.png'
 
 const About = () => {  
 
+
+  const imgStyle = { width: '15rem', height: '15rem' } 
+
   return (
     <>
-      <main className="loop-wrapper">
+      <div className="loop-wrapper">
         <div className="box">
           <div className="section-header-about section-header-create">
             <h1 className="about-title">Meet the team:</h1>    
             <br />
-            <div className="section-wrapper-about">  
-              <p className="name">Eric Petsopoulos</p>
-              <p className="about">https://github.com/ericpesto</p>
-              <p className="about">https://www.linkedin.com/in/eric-petsopoulos-b5751761/</p>
-            </div>
-            <br />
-            <div className="section-wrapper-about"> 
-              <p className="name">Sami Hakim</p> 
-              <p className="about">https://github.com/Hamisakim</p>
-              <p className="about">https://www.linkedin.com/in/samihakim/</p>
-            </div>
           </div>
-        </div>
+          <div className="section-wrapper-about">  
+            <img className='profile-pic' style={imgStyle} src={eric} alt='greek man'/>
+            <div className='about-text'>
 
-      </main>
+              <h1 className="name">Eric Petsopoulos</h1>
+              <div className="about-links">
+                <a href="https://www.linkedin.com/in/eric-petsopoulos-b5751761/">
+                  <img  src={link} alt='linkedin'/>
+                </a>
+                <a href="https://github.com/ericpesto">
+                  <img className='git-link' src={git} alt='github'/>
+                </a>
+              </div>
+            </div>
+
+          </div>
+          <br />
+
+          <div className="section-wrapper-about"> 
+            <img className='profile-pic' style={imgStyle}  src={sami} alt='arabian man'/>
+            <div className='about-text'>
+              <h1 className="name">Sami Hakim</h1> 
+
+              <div className="about-links">
+                <a href="https://www.linkedin.com/in/samihakim/"><img  src={link} alt='linkedin'/></a>
+                <a href="https://github.com/Hamisakim"> 
+                  <img className='git-link' src={git} alt='github'/>
+                </a>
+              </div>
+            </div>
+
+          </div>
+
+
+        </div>
+      </div>
       <ParticlesBg />
     </>
   )
